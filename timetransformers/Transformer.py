aef6d3228c74396f88b8c4349e9786c648688b13
tedwards2412 <tedwards2412@gmail.com>
2023-12-11 23:11:52 -0600

    Using pytroch attention module

--- timetransformers/Transformer.py
+++ timetransformers/Transformer.py
@@ -19,20 +19,20 @@ class MultiHeadAttention(nn.Module):
         self.W_v = nn.Linear(d_model, d_model)
         self.W_o = nn.Linear(d_model, d_model)
 
-    def scaled_dot_product_attention(self, Q, K, V, mask=None):
-        # Calculate attention scores
-        attn_scores = torch.matmul(Q, K.transpose(-2, -1)) / math.sqrt(self.d_k)
+    # def scaled_dot_product_attention(self, Q, K, V, mask=None):
+    #     # Calculate attention scores
+    #     attn_scores = torch.matmul(Q, K.transpose(-2, -1)) / math.sqrt(self.d_k)
 
-        # Apply mask if provided (useful for preventing attention to certain parts like padding)
-        if mask is not None:
-            attn_scores = attn_scores.masked_fill(mask == 0, -1e9)
+    #     # Apply mask if provided (useful for preventing attention to certain parts like padding)
+    #     if mask is not None:
+    #         attn_scores = attn_scores.masked_fill(mask == 0, -1e9)
 
-        # Softmax is applied to obtain attention probabilities
-        attn_probs = torch.softmax(attn_scores, dim=-1)
+    #     # Softmax is applied to obtain attention probabilities
+    #     attn_probs = torch.softmax(attn_scores, dim=-1)
 
-        # Multiply by values to obtain the final output
-        output = torch.matmul(attn_probs, V)
-        return output
+    #     # Multiply by values to obtain the final output
+    #     output = torch.matmul(attn_probs, V)
+    #     return output
 
     def split_heads(self, x):
         # Adjust to handle d_model = 1
@@ -63,7 +63,10 @@ class MultiHeadAttention(nn.Module):
         V = self.split_heads(self.W_v(V))
 
         # Perform scaled dot-product attention
-        attn_output = self.scaled_dot_product_attention(Q, K, V, mask)
+        # attn_output = self.scaled_dot_product_attention(Q, K, V, mask)
+        attn_output = nn.functional.scaled_dot_product_attention(
+            Q, K, V, attn_mask=mask
+        )
 
         # Combine heads and apply output transformation
 
